1cc20069ff935b623fe110aea26c9d56cddbd918
LAPTOP-TIF5UA39\Santiago <wilberth2010@hotmail.com>
2018-04-01 00:20:35 -0600

    s

---
+++ HealthyFood/Nutriologos/Recursos_Administrador/js/index.js
@@ -0,0 +1,6 @@
+window.onload = function(){
+        $('#preloader').delay(5000).fadeOut();
+        $('#load').delay(5050).fadeOut('slow'); 
+  $('#site-content').delay(5050).fadeIn('slow');
+}
+// Change the delay time you want:
