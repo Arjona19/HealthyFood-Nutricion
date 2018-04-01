f2f08e3d10188be5adbc9c21b8548c5c54cb7de7
LAPTOP-TIF5UA39\Santiago <wilberth2010@hotmail.com>
2018-04-01 17:48:33 -0500

    as

--- HealthyFood/Nutriologos/Recursos_Administrador/js/index.js
+++ HealthyFood/Nutriologos/Recursos_Administrador/js/index.js
@@ -1,5 +1,5 @@
 window.onload = function () {
     $('#preloader').delay(5000).fadeOut();
-  $('#load').delay(5050).fadeOut()
-  $('#site-content').delay(5050).fadeIn();
+  $('#load').delay(5050).fadeOut('slow')
+  $('#site-content').delay(5050).fadeIn('slow');
 }
