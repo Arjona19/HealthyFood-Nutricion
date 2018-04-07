9dcd5640aadf7ef15bb8e96edf53203d41e1692c
LAPTOP-TIF5UA39\Santiago <wilberth2010@hotmail.com>
2018-04-07 15:31:50 -0500

    a

---
+++ HealthyFood/Nutriologos/obj/Release/Package/PackageTmp/Recursos_Administrador/js/Acciones.js
@@ -0,0 +1,14 @@
+﻿function getDatos(id, action) {
+    $.ajax({
+        type: "POST",
+        url: action,
+        data: { id },
+        success: function (response) {
+            console.log(response);
+        }
+    });
+}
+function Toast() {
+    document.getElementById("Noti_Eliminar").classList.add("peek");
+    
+}
---
+++ HealthyFood/Nutriologos/obj/Release/Package/PackageTmp/Recursos_Administrador/js/index.js
@@ -0,0 +1,5 @@
+window.onload = function () {
+    $('#preloader').delay(5000).fadeOut();
+  $('#load').delay(5050).fadeOut('slow')
+  $('#site-content').delay(5050).fadeIn('slow');
+}
